d87038d572cbfb971dbd63da5fc6525020559aea
김병연 <harry@gimbyeoeonuiMBP.lan>
2023-05-27 13:20:28 -0400

    Commented the pointer example

--- lesson6/pointerExample.cpp
+++ lesson6/pointerExample.cpp
@@ -3,32 +3,42 @@
 int main() {
    int a = 5; 
    int * ptr = &a; 
-   std::cout << a << std::endl; 
-   std::cout << "This is the actual value " << *ptr << std::endl; 
-   std:: cout << "This is the memory address " << ptr << std::endl; 
+   std::cout << "Print out value a: " << a << std::endl; 
 
+   // '*' what the pointer is pointing at (the element)
+   std::cout << "Pointer is pointing at the value: " << *ptr << std::endl; 
+
+   // '&' the memory address of the variable a
+   std::cout << "Memory address of a: " << &a << std::endl;
+
+   // the value that the pointer contains
+   std::cout << "Memory address inside of the pointer: " << ptr << std::endl; // address of the variable a
+
+   // the memory address of the pointer
+   std::cout << "Memory address of the pointer: " << &ptr << std::endl; // different address from a
+
+   // create an array in the heap with 1, 2, 3, 4, 5
    ptr = new int[5]; 
    for (int i = 1; i < 6; i++) {
       ptr[i - 1] = i; 
    }
-   std::cout << *ptr << std::endl; 
+
+   // print out in order
+   std::cout << *ptr << " "; 
    for (int i = 1; i < 5; i++) {
       ptr++; 
-      std::cout << *ptr << std::endl; 
+      std::cout << *ptr << " "; 
    }
-   ptr++;
-   std::cout << *ptr << std::endl;
+   std::cout << std::endl; 
 
-   // for (int i = 4; i >= 0; i --) {
-   //    std::cout << ptr[i] << std::endl;
-   // }
+   // print in opposite direction
+   std::cout << *ptr << " "; 
+   for (int i = 4; i >= 1; i--) {
+      ptr--;
+      std::cout << *ptr << " "; 
+   }
+   std::cout << std::endl; 
 
-   // for (int i = 4; i >= 0; i--) {
-   //    std::cout << ptr[i] << std::endl;
-   // }
-
-   // '*' what the pointer is pointing at (the element)
-   // '&' the memory address of the element
-
+   // always free or deallocate the memory in the heap 
    delete [] ptr; 
 }
